eaa363b005f716847a58f52591d5140607bd4173
Edwin Millan-Rodriguez <bassgs3000@gmail.com>
2013-04-19 16:22:19 -0500

    Worked on the bookmarklet

---
+++ app/assets/javascripts/bookmarklet.js
@@ -0,0 +1,37 @@
+var hostname = document.location.hostname;
+switch(hostname) {
+	case 'imgur.com':
+		var imgary = [];
+		$('.album-image img').each(function(index, value){
+			imgary.push(value.src);
+		});
+
+		if (imgary.length === 0){
+			imgary.push($('#image img').attr("src"));
+		}
+		alert(JSON.stringify(imgary));
+		break;
+
+	case 'i.imgur.com':
+		var $img = $('img').src.replace("?1","");
+		alert(JSON.stringify($img));
+		break;
+
+	case 'www.reddit.com':
+		var $image = $('.last-clicked .thumbnail').attr('href');
+		if (/\.(jpg|png|gif)/.test($image)) { 
+			alert(JSON.stringify($image));
+		}
+		else {
+			if (/http:\/\/imgur.com/.test($image)){
+				alert("Please Velocilink me from within the imgur page");
+			}
+			else {
+				alert("Not a valid image: " + $image);
+			}
+		}
+		break;
+
+	default:
+		alert("Site not supported!");
+}
